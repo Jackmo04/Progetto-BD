70a9f4e84ee9365cbc1e810ddfca4b37e2087b8f
Matteo Grandini <matteo.grandini@studio.unibo.it>
2025-05-23 19:21:36 +0200

    Aggiornato popolamento db

--- db-main/PROGETTO.sql
+++ db-main/PROGETTO.sql
@@ -29,7 +29,7 @@ create table AREA_ATTRACCO (
      constraint ID_AREA_ATTRACCO_ID primary key (CodArea));
 
 create table ASTRONAVE (
-     Targa char(10) not null,
+     Targa char(8) not null,
      Nome varchar(20) not null,
      CodArea INTEGER,
      NumeroPosto INTEGER,
@@ -65,7 +65,7 @@ create table MODELLO (
      constraint ID_MODELLO_ID primary key (CodModello));
 
 create table PERSONA (
-     CUI char(20) not null,
+     CUI char(13) not null,
      Username varchar(20) not null,
      Nome varchar(25) not null,
      Cognome varchar(25) not null,
@@ -80,7 +80,7 @@ create table PERSONA (
      constraint SID_PERSONA_ID unique (Username));
 
 create table PIANETA (
-     CodPianeta char(20) not null,
+     CodPianeta char(10) not null,
      Nome varchar(25) not null,
      constraint ID_PIANETA_ID primary key (CodPianeta));
 
@@ -277,3 +277,111 @@ create unique index ID_TIPOLOGIA_CARICO_IND
 create unique index ID_TIPOLOGIA_VIAGGIO_IND
      on TIPOLOGIA_VIAGGIO (CodTipoViaggio);
 
+-- Insertions Section
+-- _____________ 
+
+INSERT INTO PIANETA (CodPianeta, Nome) VALUES
+('DTHSTR0', 'Morte Nera'),
+('CORU001', 'Coruscant'),
+('TATO002', 'Tatooine'),
+('HOTH003', 'Hoth'),
+('NABO004', 'Naboo'),
+('ALDE005', 'Alderaan'),
+('KASH006', 'Kashyyyk'),
+('MUST007', 'Mustafar'),
+('GEON008', 'Geonosis'),
+('ENDOR09', 'Endor'),
+('DANT010', 'Dantooine');
+
+INSERT INTO DIMENSIONE_PREZZO (Superficie, Prezzo) VALUES
+(50, 100.00),
+(100, 180.00),
+(200, 350.00),
+(500, 800.00),
+(1000, 1500.00);
+
+INSERT INTO MODELLO (CodModello, Nome, DimensioneArea) VALUES
+('XW0001', 'X-Wing', 50),
+('MF0002', 'Millennium Falcon', 100),
+('SD0003', 'Star Destroyer', 1000),
+('TIF004', 'TIE Fighter', 50),
+('CR9005', 'Corvette CR90', 200),
+('GR7506', 'Trasporto GR-75', 200);
+
+
+INSERT INTO CELLA (NumCella, Capienza) VALUES
+(1, 5),
+(2, 3),
+(3, 10),
+(4, 2),
+(5, 7);
+
+INSERT INTO PERSONA (CUI, Username, Nome, Cognome, Razza, DataNascita, Ricercato, Ideologia, Ruolo, NumCella, PianetaNascita) VALUES
+('SKWLKE510925T', 'L.Skywalker', 'Luke', 'Skywalker', 'Umano', '1951-09-25', TRUE, 'Ribelle', 'Astronauta', NULL, 'TATO002'),
+('SLOHAN420713C', 'H.Solo', 'Han', 'Solo', 'Umano', '1942-07-13', FALSE, 'Neutrale', 'Capitano', NULL, 'CORU001'),
+('RGNLLA510925A', 'L.Organa', 'Leia', 'Organa', 'Umano', '1951-09-25', TRUE, 'Ribelle', 'Astronauta', 4, 'ALDE005'),
+('SKWNKN410419T', 'D.Vader', 'Anakin', 'Skywalker', 'Umano', '1941-04-19', FALSE, 'Imperiale', 'Capitano', NULL, 'TATO002'),
+('PLPSHV201204N', 'E.Palatine', 'Sheev', 'Palpatine', 'Umano', '1920-12-04', FALSE, 'Imperiale', 'Admin', NULL, 'NABO004'),
+('CHWBCC000101K', 'Chewie', 'Chewbacca', 'unknow', 'Wookiee', '1900-01-01', TRUE, 'Ribelle', 'Astronauta', NULL, 'KASH006'),
+('KNBOBI370825C', 'O.Kenobi', 'Obi-Wan', 'Kenobi', 'Umano', '1937-08-25', TRUE, 'Ribelle', 'Astronauta', NULL, 'CORU001'),
+('MULDRT600322D', 'D.Maul', 'Darth', 'Maul', 'Zabrak', '1960-03-22', FALSE, 'Imperiale', 'Capitano', NULL, 'DANT010'),
+('TRKMFF220306M', 'G.Tarkin', 'Moff', 'Tarkin', 'Umano', '1922-03-06', FALSE, 'Imperiale', 'Admin', NULL, 'MUST007'),
+('STRMTR0000000', 'Trooper0', 'Stormtrooper', '00000', 'Clone', '', FALSE, 'Imperiale', 'Astronauta', NULL, 'DTHSTR0'),
+('STRMTR0000001', 'Trooper1', 'Stormtrooper', '00001', 'Clone', '', FALSE, 'Imperiale', 'Astronauta', NULL, 'DTHSTR0'),
+('STRMTR0000002', 'Trooper2', 'Stormtrooper', '00002', 'Clone', '', FALSE, 'Imperiale', 'Astronauta', NULL, 'DTHSTR0'),
+('STRMTR0000003', 'Trooper3', 'Stormtrooper', '00003', 'Clone', '', FALSE, 'Imperiale', 'Astronauta', NULL, 'DTHSTR0');
+
+INSERT INTO AREA_ATTRACCO (CodArea, Nome) VALUES
+(1, 'Alpha'),
+(2, 'Beta'),
+(3, 'Officina'),
+(4, 'Rifornimento');
+
+INSERT INTO POSTEGGIO (CodArea, NumeroPosto) VALUES
+(1, 1), (1, 2), (1, 3), (1, 4), (1, 5),
+(2, 1), (2, 2), (2, 3), (2, 4),
+(3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6),
+(4, 1), (4, 2);
+
+INSERT INTO ASTRONAVE (Targa, Nome, CodArea, NumeroPosto, CodModello, CUICapitano) VALUES
+('MFALC001', 'Millennium Falcon', 2, 1, 'MF0002', 'SLOHAN420713C'),
+('XWING002', 'Red Five', 3, 1, 'XW0001', 'MULDRT600322D'),
+('STARD003', 'Executor', 3, 5, 'SD0003', 'MULDRT600322D'),
+('CR900004', 'Tantive IV', 4, 1, 'CR9005', 'MULDRT600322D'),
+('TIEF0005', 'Black Squadron 1', 3, 2, 'TIF004', 'SKWNKN410419T');
+
+INSERT INTO EQUIPAGGIO (CUIAstronauta, TargaAstronave) VALUES
+('STRMTR0000002', 'XWING002'),
+('SKWLKE510925T', 'MFALC001'),
+('STRMTR0000003', 'CR900004'),
+('STRMTR0000001', 'STARD003'),
+('CHWBCC000101K', 'MFALC001'),
+('STRMTR0000001', 'CR900004'),
+('STRMTR0000000', 'TIEF0005');
+
+INSERT INTO TIPOLOGIA_VIAGGIO (CodTipoViaggio, Nome) VALUES
+(1, 'Trasporto merci'),
+(2, 'Missione diplomatica'),
+(3, 'Pattugliamento imperiale'),
+(4, 'Trasporto prigionieri'),
+(5, 'Esplorazione');
+
+INSERT INTO TIPOLOGIA_CARICO (CodTipoCarico, Nome, Descrizione, CostoUnitario) VALUES
+(1, 'Spezie', 'Carico di spezie', 500.00),
+(2, 'Armi', 'Armi imperiali', 1200.00),
+(3, 'Dati', 'Informazioni classificate', 2000.00),
+(4, 'Droidi', 'Componenti per droidi', 150.00),
+(5, 'Alimentari', 'Razioni standard', 50.00);
+
+INSERT INTO RICHIESTA (CodRichiesta, EntrataUscita, DataOra, Descrizione, CostoTotale, Esito, DataEsito, TargaAstronave, Scopo, PianetaProvenienza, PianetaDestinazione, GestitaDa) VALUES
+(1, 'E', '2025-05-20 10:00:00', 'Arrivo per scarico spezie', 1680.00, 'R', '2025-05-20',  'MFALC001', 1, 'TATO002', 'DTHSTR0', 'TRKMFF220306M'),
+(2, 'U', '2025-05-21 14:30:00', 'Partenza per missione diplomatica', 350.00, 'A', '2025-05-21',  'CR900004', 2, 'DTHSTR0', 'ALDE005', 'PLPSHV201204N'),
+(3, 'E', '2025-05-22 08:00:00', 'Rifornimento e manutenzione', 1600.00, NULL, NULL, 'XWING002', 4, 'HOTH003', 'DTHSTR0', NULL),
+(4, 'U', '2025-05-22 18:00:00', 'Pattuglia settore 7', 1500.00, 'A', '2025-05-22', 'STARD003', 3, 'DTHSTR0', 'GEON008', 'PLPSHV201204N'),
+(5, 'E', '2025-05-23 09:15:00', 'Arrivo carico armi', 5300.00, NULL, NULL, 'TIEF0005', 1, 'NABO004', 'DTHSTR0', NULL);
+
+INSERT INTO CARICO (Tipologia, Quantita, CodRichiesta) VALUES
+(1, 3, 1),
+(2, 1, 5),
+(3, 2, 5),
+(4, 10, 3);
