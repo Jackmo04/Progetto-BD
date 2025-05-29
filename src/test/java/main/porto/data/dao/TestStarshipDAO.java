8a4948cafdacd374125f25020217b46ea2d991a6
Matteo Grandini <matteo.grandini@studio.unibo.it>
2025-05-29 18:15:10 +0200

    Fixed test

--- src/test/java/main/porto/data/dao/TestStarshipDAO.java
+++ src/test/java/main/porto/data/dao/TestStarshipDAO.java
@@ -1,18 +1,37 @@
 package main.porto.data.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Savepoint;
+import java.util.Set;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import porto.data.ParkingAreaImpl;
+import porto.data.ParkingSpaceImpl;
+import porto.data.PersonImpl;
+import porto.data.ShipModelImpl;
+import porto.data.StarshipImpl;
+import porto.data.dao.StarshipDAOImpl;
+import porto.data.utils.DAOUtils;
+
 class TestStarshipDAO {
 
     private static Connection connection;
     private static Savepoint savepoint;
 
-    @BeforeClass
+    @BeforeAll
     public static void setup() throws SQLException {
         connection = DAOUtils.localMySQLConnection("PortoMorteNera", "root", "");
         connection.setAutoCommit(false);
         savepoint = connection.setSavepoint();
     }
 
-    @AfterClass
+    @AfterAll
     public static void cleanup() throws SQLException {
         if (connection != null) {
             if (savepoint != null) {
@@ -41,7 +60,7 @@ class TestStarshipDAO {
                 new PersonImpl("STRMTR0000001", "Pippo", "Pluto")
             )
         );
-        assertThat(actual).hasSameElementsAs(expected);
+        assertEquals(expected, actual);
     }
 
 }
